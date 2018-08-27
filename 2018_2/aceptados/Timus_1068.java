4c620b3af8a39a841f1c7330737591283d01d351
Pablo <41352808+floghos@users.noreply.github.com>
2018-08-27 10:51:17 -0300

    Add files via upload

---
+++ 2018_2/aceptados/Timus_1068.java
@@ -0,0 +1,19 @@
+package timus_1068;
+
+import java.util.Scanner;
+
+public class Timus_1068 {
+
+    public static void main(String[] args) {
+        int n, dir, i;
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        dir = (n <  0)? -1 : 1;
+        for (i = 1; i != (n+dir); i += dir) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+}
