13b272709f42553aca9479ffd2879d7327b2f2ca
Pablo <41352808+floghos@users.noreply.github.com>
2018-08-27 14:39:04 -0300

    Add files via upload

---
+++ 2018_2/aceptados/primer_set/Timus_1409.java
@@ -0,0 +1,13 @@
+package timus_1409;
+
+import java.util.Scanner;
+
+public class Timus_1409 {
+    public static void main(String[] args) {
+        int harry, larry;
+        Scanner scan = new Scanner(System.in);
+        harry = scan.nextInt();
+        larry = scan.nextInt();
+        System.out.println((larry - 1) + " " + (harry - 1));
+    }
+}
