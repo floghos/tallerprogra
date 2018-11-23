6cde6757902ce32d4048d3157160068646812758
Pablo <41352808+floghos@users.noreply.github.com>
2018-11-23 00:34:33 -0300

    Add files via upload

--- 2018_2/in_progress/UVa11572.cpp
+++ 2018_2/in_progress/UVa11572.cpp
@@ -2,27 +2,17 @@
 
 using namespace std;
 
-int customBinarySearch(vector<int> arr, int lower, int upper, int target) {
-	//if not found, returns the closest number that is lower than the target
-	if (upper >= lower) {
-		int mid = lower + (upper - lower)/2;
-
-		if (arr[mid] == target) {
-			return mid;
-		}
-		if (arr[mid] < target){
-			return customBinarySearch(arr, mid+1, upper, target);
-		}
-		return customBinarySearch(arr, lower, mid-1, target);
+int search(vector<int> arr, int begin, int end, int target) {
+	for (int i = 0; i < (end-begin+1); ++i) {
+		if (arr[begin + i] == target) return begin + i;
 	}
-	return upper;
+	return -1;
 }
 
 int main(int argc, char const *argv[]) {
 	int cases;
 	vector<int> output;
 	vector<int> sflakes;
-	//vector<int> bag;
 	int maxBag;
 
 	cin >> cases;
@@ -40,20 +30,19 @@ int main(int argc, char const *argv[]) {
 		int begin, end, repeat;
 		begin = 0;
 		end = 0;
-
+		
 		if (sflakes.size() == 1) {
 			output.push_back(1);
 		} else {
 			for (int j = 1; j < sflakes.size(); ++j) {
-				repeat = customBinarySearch(sflakes, begin, end, sflakes[j]);
+				repeat = search(sflakes, begin, end, sflakes[j]);
 				end = j; //set the new ending index
-				if (repeat < 0 || sflakes[repeat] != sflakes[j]) { //if sflakes[j] was not found between begin and end
+				if (repeat == -1) { //if sflakes[j] was not found between begin and end
 					if ((end-begin+1) > maxBag) {
 						maxBag = end - begin + 1;
 					}
-				} else if (sflakes[repeat] == sflakes[j]) {
+				} else { //if sflakes[j] was found
 					begin = repeat + 1; //set the new starting index
-					// end = j; //set the new ending index
 					if ((end-begin+1) > maxBag) {
 						maxBag = end - begin + 1;
 					}
