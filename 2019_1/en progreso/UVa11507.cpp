ef257b76e095271f3df543a8b5975cf51fff64a3
floghos <caleuche94@gmail.com>
2019-04-10 00:20:31 -0400

    some base stuff for problem 11507

---
+++ 2019_1/en progreso/UVa11507.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int l;
+
+	cin >> l;
+
+	while (l != 0) {
+		
+		for (int i = 0; i < l - 1; i++) {
+			char dir, axis, blank;
+			cin >> dir >> axis >> blank;
+			if (dir == 'N') continue;
+
+
+		}
+
+
+
+		cin >> l;
+	}
+
+	return 0;
+}
